75e72067db04af616149dde433f938e5a4c32614
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-02-28 11:39:44 -0600

    Ejercicio propio para la lectura y escritura de arreglos

--- Arreglos/src/arrays/Arreglo.java
+++ Arreglos/src/arrays/Arreglo.java
@@ -1,25 +1,31 @@
 package arrays;
-
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Arreglo {
 	public static void main(String[] args) {
 		//We ask user how many elements the array will have
 		
-		System.out.println("Ingrese la cantidad de elementos que tendrá el array");
-		int sizeOfArray=5;
+		System.err.println("Ejercicios y pruebas con arrays en java");
+		
+		//We define the array's length 
+		int sizeOfArray=4;
+		
+		ArregloEntradaEstandar arr=new ArregloEntradaEstandar();
+		
+		
+//		ArregloEntradaEstandar arr_2=new ArregloEntradaEstandar(sizeOfArray);
+		
+		
+		
+
+		
+		
+		
+		
+		
 		
 //		Scanner scanner=new Scanner(System.in);
 //		int sizeOfArray=scanner.nextInt();
 //		scanner.close();
-		
-		System.out.println("El arreglo tendrá la dimensión de "+sizeOfArray+" elementos.");
-		ArregloEntradaEstandar arr1 =new ArregloEntradaEstandar(sizeOfArray);
-//		ArregloEntradaEstandar arr1 =new ArregloEntradaEstandar(5);
-		
-		
-		
-		
-		
 	}
 }
--- Arreglos/src/arrays/ArregloEntradaEstandar.java
+++ Arreglos/src/arrays/ArregloEntradaEstandar.java
@@ -9,50 +9,52 @@ public class ArregloEntradaEstandar{
 	//The default length of the array is 5
 	public ArregloEntradaEstandar() {
 		this.sizeOfArray=5;
-		getArrayInfo(this.sizeOfArray);
+		createArray(this.sizeOfArray);
 	}
 	
 	//A custom length value given by an user
 	 public ArregloEntradaEstandar(int sizeOfArray) {
 		this.sizeOfArray=sizeOfArray;
-		getArrayInfo(this.sizeOfArray);
+		createArray(this.sizeOfArray);
 		
 	}
 	
 	//-----------------------------------Two Public Methods
-	public static void getArrayInfo(int sizeOfArray) {
+	public static void createArray(int sizeOfArray) {
 		//We declare the array
 		int arr[]=new int[sizeOfArray];
 		int i=0; 
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Ingresa un valor");
-		int v = scanner.nextInt();
-
 		
-		do {
-			System.out.println("Ingrese el valor del arr["+i+"]");
-			v=scanner.nextInt();
+		Scanner scanner=new Scanner(System.in);
+	
+
+		while (i<sizeOfArray) {
+			System.out.println("Ingresa elemento de array ["+i+"]: ");
+			arr[i]=scanner.nextInt();;			
+//			System.err.println("A["+i+"]="+arr[i]);
 			i++;
 			
-			System.err.println("=====================================");
-			System.err.println("El valor leído es: "+v);
-			System.err.println("El índice vale: "+i);
-			System.err.println("Queremos llegar a: "+sizeOfArray);
-			System.err.println("=====================================");
-		}while(i<sizeOfArray);
+		}
 		scanner.close();
-
-			
+		//We show array in console
+		getArrayInfo(arr);
 		
-		/*for(int j=0; j<i;j++) {
-			System.out.println("El arr quedó como: ");
-			System.out.println(arr[j]);
-		}*/
+	 }
+	
+	
+	public static void getArrayInfo(int[] arr) {
+		System.err.println("================================================");
+		System.err.println("El array que ingresaste es: ");
+		System.err.println("================================================");
 		
-		
-		
+		for(int j=0;j<arr.length;j++) {
+			System.out.println("arr["+j+"]="+arr[j]);
+		}
 	}
 	
+
+	
+		
 	
 
 }
