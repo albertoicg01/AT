8b483ee80af2894407c4b357361185f3959d0e23
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-02-28 18:09:36 -0600

    Actualizacion

--- EjercicioMario/src/ejercicios/Ejercicios.java
+++ EjercicioMario/src/ejercicios/Ejercicios.java
@@ -3,13 +3,15 @@ package ejercicios;
 public class Ejercicios {
 	public static void main(String[] args) {
 		ejercicio1();
+		ejercicio2();
+		ejercicio3();
 		
 	}
 	
 	
 	public static void ejercicio1() {
 		int a=10, b=20, c=30;
-		
+		System.err.println("=============Operaciones verdadero o falso");
 //		Si a = 10, b = 20, c = 30, entonces:
 //			(a + b) > c
 //			(a – b) < c
@@ -74,11 +76,16 @@ public class Ejercicios {
 		System.err.println("=============Expresiones lógicas");
 		
 		
-		System.out.println(21%7);
-		System.out.println(9/2 + 15 % 2);
-		System.out.println((3+6)/2 + 1.5);
-		System.out.println((10+38/(14-(10-12/(2*3)))));
-		System.out.println((10+38/(14-(10-12/(2*3)))));
+		System.out.println(!(5>6)&&7<=4);
+		System.out.println(7>4 && 5<=5 || 4==5);
+		System.out.println(! (7==7) && (7>=8 || 8==6));
+		System.out.println((5+2)<=5 && 3*2==5 || 7<=2*2 || 2*2<=(2+2));
+		System.out.println(((! (14/2>8) || 5>5) && (5<=27/3 || 5+3<=3/2)));
+		System.out.println((3+5*2)==12/3 && ((5+3)==18/9 || 10/2<=9) || !(9>=2));
+		
+
+
+
 		
 	}
 	
