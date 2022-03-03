d6b91753f5fff572dfeef581f160bc4b964565d5
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-03-03 12:53:28 -0600

    Se hace actualizacion para duda en clase Arreglo

--- Arreglos/src/arrays/Arreglo.java
+++ Arreglos/src/arrays/Arreglo.java
@@ -1,31 +1,43 @@
 package arrays;
+import java.util.Scanner;
+
 //import java.util.Scanner;
 
 public class Arreglo {
 	public static void main(String[] args) {
-		//We ask user how many elements the array will have
-		
-		System.err.println("Ejercicios y pruebas con arrays en java");
-		
-		//We define the array's length 
-		int sizeOfArray=4;
-		
-		ArregloEntradaEstandar arr=new ArregloEntradaEstandar();
-		
-		
-//		ArregloEntradaEstandar arr_2=new ArregloEntradaEstandar(sizeOfArray);
-		
-		
-		
-
-		
-		
-		
-		
-		
-		
+		arrays();
 //		Scanner scanner=new Scanner(System.in);
 //		int sizeOfArray=scanner.nextInt();
 //		scanner.close();
+	}
+	
+	public static void arrays() {
+		//We ask user how many elements the array will have
+		System.out.println("Ejercicios y pruebas con arrays en java");
+		
+		//We define the array's length 
+		//1 dimension array
+		//========================================================================
+		System.out.println("Ingrese la dimensión del arreglo");
+		Scanner scanner=new Scanner(System.in);
+		int sizeOfArray=scanner.nextInt();
+		//scanner.close(); 
+		
+		ArregloEntradaEstandar arr_2=new ArregloEntradaEstandar(sizeOfArray);
+		
+	}
+	
+	public static void matrix2_2() {
+		//Matrixs
+		//========================================================================
+		System.err.println("*");
+		System.err.println("*");
+		System.err.println("*");
+		System.out.println("Matrices");
+		
+		
+		System.out.println("Ingresa cantidad de renglones: ");
+		System.out.println("Ingresa cantidad de columnas: ");
+		
 	}
 }
