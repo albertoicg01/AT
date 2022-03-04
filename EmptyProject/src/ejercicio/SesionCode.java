d751d053b94de5ccc8a8b8d7955bb389556ac5fd
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-03-04 16:22:31 -0600

    Proyecto Auxiliar para tomar notas

---
+++ EmptyProject/src/ejercicio/SesionCode.java
@@ -0,0 +1,11 @@
+package ejercicio;
+
+public class SesionCode {
+	public static void sesionCode() {
+		System.out.println("Codigo de sesion- Flujos de control -Bucles Ciclos- Loops");
+		System.out.println("While");
+		System.out.println("Do while");
+		System.out.println("For");
+		
+	}
+}
---
+++ EmptyProject/src/ejercicio/main.java
@@ -0,0 +1,15 @@
+package ejercicio;
+
+import java.util.Scanner;
+
+public class main extends SesionCode{
+	public static void main(String[] args) {
+		System.out.println("Ejercicio de 4 de marzo de 2022");
+//		Scanner scanner = new Scanner(System.in);
+		
+		sesionCode();
+		
+		
+	}
+
+}
