55ad93e5944cbec559198fe3b22e852e40143b98
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-03-07 15:29:13 -0600

    Entorno de estudio de fechas y horas

--- EmptyProject/src/ejercicio/main.java
+++ EmptyProject/src/ejercicio/main.java
@@ -4,13 +4,7 @@ import java.util.Scanner;
 
 public class main extends SesionCode{
 	public static void main(String[] args) {
-		System.out.println("Ejercicio de 4 de marzo de 2022");
-//		Scanner scanner = new Scanner(System.in);
-		
-		//sesionCode();
-		sesionCode2();
-		
-		
-	}
 
+
+	}
 }
---
+++ FechasHoras/src/fechas_horas/Main.java
@@ -0,0 +1,16 @@
+package fechas_horas;
+
+
+
+public class Main extends SesionCode{
+	public static void main(String[] args) {
+		System.out.println("Ejercicio de 4 de marzo de 2022");
+//		Scanner scanner = new Scanner(System.in);
+		
+		//sesionCode();
+		sesionCode2();
+		
+		
+	}
+
+}
