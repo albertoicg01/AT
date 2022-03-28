0d9f838abfb6afb4c1ee4c19f3343688707245bc
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-03-28 09:17:13 -0600

    Program and practice with Date class is still in process

--- Practice/src/practice/Main.java
+++ Practice/src/practice/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 //Date is a library about to be deleted by new Java Versions
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.time.Month;
 
 public class Main {
 	public static void main(String[] args) {
@@ -11,31 +12,73 @@ public class Main {
 //		Scanner scanner=new Scanner(System.in);
 		Date date=new Date();
 		
-		System.out.println("Today is "+date);
-		
 		//SimpleDateFormat is a class where you can pass 
-		//the format you'll want to show the date
+		//the format you'll want to show the date object
 		
 		SimpleDateFormat dateFormat=new SimpleDateFormat("YYYY-MM-DD");
 		
 		System.out.println("Today is: "+dateFormat.format(date));
 		
-
 		
+		System.out.println("====================================================");
+		System.out.println("Choose an option (type only a number) ");
+		System.out.println("1.- Do you know his/her birthdays's date?");
+		System.out.println("2.- Do you know when is the date when that person born?");
+		
+		
+		Scanner scanner =new Scanner(System.in);
+		int option=0;
+		
+		try {
+			option=scanner.nextInt();			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
 	
 		
-		
-		
-		
-		
+		if(option==1) {
+			getBornDate(date,dateFormat);
+		}else if(option==2){
+			System.out.println("2 was chosen");
+		}else {
+			System.out.println("Option not valid");
+		}
+			
 	}
 	
 	
 	
-	public static void getBirthday() {
+	public static void getBornDate(Date currentDate, SimpleDateFormat dateFormat)  {
 		//You type your birthday and it gives the date you born 
+		Scanner scanner=new Scanner(System.in);	
+		System.out.println("------------------------------------");		
+		System.out.println("Type his/her birthday's ");
 		
+		System.out.println("Type his/her month");
+		String auxiliarMonth=scanner.next();
+		
+		System.out.println("Type his/her day");
+		String auxiliarDay=scanner.next();
+		
+		System.out.println("We recieved MONTH: "+auxiliarMonth);
+		System.out.println("We recieved DAY: "+auxiliarDay);
+		
+		
+		//We get current year
+		System.out.println("TODAY IS: "+dateFormat.format(currentDate));
+		
+		//We know the 
+//		System.out.println("Your birthday is on "+birthdayDate);
+		
+		
+		
+		System.out.println("------------------------------------");
+		
+	}
+	
+	public static void getBirthDate(){
+		//Type your born's date and this method shows your born's date
 		
 	}
 }
