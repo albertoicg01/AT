bfab7d107bf80250806de21282a9e2d6b92bee77
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-04-05 06:12:20 -0500

    ShowElapsedTime method is integrated successfully

--- Hilos/src/activityDone/Main.java
+++ Hilos/src/activityDone/Main.java
@@ -1,20 +1,20 @@
 package activityDone;
+import java.io.*;
 
 public class Main {
+	
+	
 	public static void main(String[] args) {
 		
 		
-		UsingThread usingThread = new UsingThread(secondsToMiliseconds(15));
-		UsingThread usingThread2 = new UsingThread(secondsToMiliseconds(10));
-		UsingThread usingThread3= new UsingThread(secondsToMiliseconds(5));
-		UsingThread usingThread4 = new UsingThread(secondsToMiliseconds(3));
-		UsingThread usingThread5 = new UsingThread(secondsToMiliseconds(2));
+		UsingThread usingThread = new UsingThread(secondsToMiliseconds(20));
 		
-		usingThread.setName("Hilo1");
-		usingThread2.setName("Hilo2");
-		usingThread3.setName("Hilo3");
-		usingThread4.setName("Hilo4");
-		usingThread5.setName("Hilo5");
+		UsingThread usingThread2 = new UsingThread(secondsToMiliseconds(15));
+		UsingThread usingThread3= new UsingThread(secondsToMiliseconds(10));
+		UsingThread usingThread4 = new UsingThread(secondsToMiliseconds(5));
+		UsingThread usingThread5 = new UsingThread(secondsToMiliseconds(1));
+		
+
 		
 		
 		usingThread.start();
@@ -22,14 +22,33 @@ public class Main {
 		usingThread3.start();
 		usingThread4.start();
 		usingThread5.start();
-				
+		
 	}
 	
+	
+	//-----------------------------------------------------------------------
+	//Here we calculate time
+	static long baseTime = System.currentTimeMillis();
+    // show the time elapsed since the program started
+
+    static void showElapsedTime(String message) {
+	long elapsedTime = System.currentTimeMillis() -baseTime;
+	System.out.println(message + " at " + (elapsedTime/1000.0) + " seconds");
+    }
+	//-----------------------------------------------------------------------
+    
 	public static int secondsToMiliseconds(int seconds) {
 		int miliseconds=1000;
 		miliseconds*=seconds;
 		return miliseconds;
 		
+	}
+	
+	
+	public static long differenceTime(long initial_time,long final_time) {
+		
+		long time_difference=final_time-initial_time;
+		return time_difference;
 	}
 }
 
@@ -37,24 +56,26 @@ public class Main {
 class UsingThread extends Thread{
 	int timeSleep;
 	
-	//Constructor Method
-	public UsingThread(){
-			
-	}
-	
+	//Constructor
 	public UsingThread(int timeSleep){
+		//This pause is needed
+		Main.showElapsedTime(getName()+" is created");
+		
 		this.timeSleep=timeSleep;		
 	}
 	
 	@Override
 	public void run() {
+		
 		try {
 			//sleep(10000/2);
 			sleep(this.timeSleep);
+			
+			Main.showElapsedTime(getName()+" finishes");
 		}catch(Exception e) {
 			e.getMessage();
 		}
 		
-		System.out.println(getName());
+
 	}
 }
