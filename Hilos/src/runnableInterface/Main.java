7a0d739f612b31aaeaf0cb736d88cf9b70ecbab6
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-04-05 19:51:16 -0500

    An example with Runnable interface is added

---
+++ Hilos/src/runnableInterface/Main.java
@@ -0,0 +1,48 @@
+package runnableInterface;
+
+public class Main {
+	public static void main(String[] args) {
+		Thread thread1 =new Thread(new RunnableThread("HiloRunnable_1"));
+		
+		thread1.start();
+	
+	}
+	
+	
+	
+	static long baseTime = System.currentTimeMillis();
+	
+	static void showElapsedTime(String message) {
+		long elapsedTime = System.currentTimeMillis() -baseTime;
+		System.out.println(message + " at " + (elapsedTime/1000.0) + " seconds");
+	}
+	
+}
+
+
+
+class RunnableThread implements Runnable{
+	
+	protected String threadName;
+	protected int seconds;
+	
+	
+	
+	public RunnableThread(String name) {
+		this.threadName=name;
+		Main.showElapsedTime(threadName+" is created");
+	}
+	
+	public void run() {
+		Main.showElapsedTime("RunnablePseudoIO starts");
+		try {
+			Thread.sleep(10000);  // 10 seconds
+		    
+		    Main.showElapsedTime("RunnablePseudoIO finishes");
+		}catch(Exception e) {
+			e.getMessage();
+		}
+	
+		
+	}
+}
